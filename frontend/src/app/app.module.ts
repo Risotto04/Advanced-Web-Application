761fd4f92da86db209337e437fb5b0c3f2beb89e
Theerachot <b.theerachot891@hotmail.com>
2024-09-18 15:22:36 +0700

    Folder Frontend Structure

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,37 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormComponentComponent } from './components/form-component/form-component.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { ExerciseTrackerComponent } from './components/exercise-tracker/exercise-tracker.component';
-import { MealTrackerComponent } from './components/meal-tracker/meal-tracker.component';
-import { SleepTrackerComponent } from './components/sleep-tracker/sleep-tracker.component';
-import { UserProfileComponent } from './components/user-profile/user-profile.component';
-import { SummaryChartComponent } from './components/summary-chart/summary-chart.component';
-import { SettingsComponent } from './components/settings/settings.component';
-import { ReactiveFormsModule } from '@angular/forms';
+
+import { SharedModule } from '@shared/shared.module';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    FormComponentComponent,
-    HeaderComponent,
-    FooterComponent,
-    DashboardComponent,
-    ExerciseTrackerComponent,
-    MealTrackerComponent,
-    SleepTrackerComponent,
-    UserProfileComponent,
-    SummaryChartComponent,
-    SettingsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
