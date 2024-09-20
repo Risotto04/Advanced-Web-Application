0953eda38deecb3d50bce268bf4dadb218c687a5
Theerachot <b.theerachot891@hotmail.com>
2024-09-20 21:49:10 +0700

    Add NavComponent ,  Spartan

--- frontend/src/app/layout/content-layout/content-layout.component.ts
+++ frontend/src/app/layout/content-layout/content-layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { FooterComponent } from '../footer/footer.component';
+import { NavComponent } from '../nav/nav.component';
+import { AppRoutingModule } from '../../app-routing.module';
 @Component({
+  standalone: false,
   selector: 'app-content-layout',
   templateUrl: './content-layout.component.html',
-  styleUrl: './content-layout.component.css'
+  styleUrl: './content-layout.component.css',
 })
-export class ContentLayoutComponent {
-
-}
+export class ContentLayoutComponent {}
--- frontend/src/app/modules/home/home.component.ts
+++ frontend/src/app/modules/home/home.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-
+import { HlmButtonDirective } from '@shared/shared.module';
 @Component({
+  // standalone: true,
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
