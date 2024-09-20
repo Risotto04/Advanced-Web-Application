7873f9a155fa8f401c631c5b9bae22481f8d2764
Theerachot <b.theerachot891@hotmail.com>
2024-09-20 22:52:53 +0700

    add FooterComponent

--- frontend/src/app/layout/content-layout/content-layout.component.ts
+++ frontend/src/app/layout/content-layout/content-layout.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { FooterComponent } from '../footer/footer.component';
-import { NavComponent } from '../nav/nav.component';
-import { AppRoutingModule } from '../../app-routing.module';
+
 @Component({
   standalone: false,
   selector: 'app-content-layout',
