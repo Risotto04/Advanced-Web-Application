f88bfc2707439b03c3c4119bb7a2a49c1c1f2556
Theerachot <b.theerachot891@hotmail.com>
2024-09-21 15:35:58 +0700

    Add Home Page

--- frontend/src/app/modules/home/home.component.ts
+++ frontend/src/app/modules/home/home.component.ts
@@ -6,4 +6,12 @@ import { HlmButtonDirective } from '@shared/shared.module';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  items = [
+    { img: 'images/test/1.webp', category: 'Fresh Flowers' },
+    { img: 'images/test/2.webp', category: 'Dried Flowers' },
+    { img: 'images/test/3.webp', category: 'Live Plants ' },
+    { img: 'images/test/4.webp', category: 'Aroma Candels' },
+    { img: 'images/test/5.webp', category: 'Fresheners' },
+  ];
+}
