7f5139e2adb2b0dd485ff7f98ba0faa9d6eb52df
Phisit <150761474+Phisit-pool@users.noreply.github.com>
2024-09-21 23:36:45 +0700

    update nav and footer

--- frontend/src/app/layout/nav/nav.component.ts
+++ frontend/src/app/layout/nav/nav.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   standalone: false,
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
-export class NavComponent {}
+export class NavComponent {
+  constructor(private router: Router) {}
+
+  scrollToContact() {
+    this.router.navigate(['/home']).then(() => {
+
+      const element = document.getElementById('contact');
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  }
+}
