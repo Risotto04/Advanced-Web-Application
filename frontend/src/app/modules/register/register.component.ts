2425e29a6e53e19d50f0c35c7bde8dd0fd5c2ffa
Autsada555 <autsadaheha@gmail.com>
2024-09-21 17:14:23 +0700

    updateRegister

--- frontend/src/app/modules/register/register.component.ts
+++ frontend/src/app/modules/register/register.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,26 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  firstname: string = '';
-  lastname: string = '';
-  email: string = '';
-  password: string = '';
-  phone_number: string = '';
 
-  constructor(private http: HttpClient) { }
-
-  onRegister() {
-    const userData = {
-      firstname: this.firstname,
-      lastname: this.lastname,
-      email: this.email,
-      password: this.password,
-      phone_number: this.phone_number,
-    };
-
-    this.http.post('http://localhost:3000/register', userData).subscribe(
-      response => console.log('Registration successful', response),
-      error => console.error('Registration error', error)
-    );
-  }
 }
