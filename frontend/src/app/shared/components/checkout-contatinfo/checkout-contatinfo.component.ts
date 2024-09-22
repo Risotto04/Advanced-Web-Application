2cb2c16a635221051e7d4f3624266ae886033d20
Autsada555 <autsadaheha@gmail.com>
2024-09-23 03:56:21 +0700

    child to parent Demo

--- frontend/src/app/shared/components/checkout-contatinfo/checkout-contatinfo.component.ts
+++ frontend/src/app/shared/components/checkout-contatinfo/checkout-contatinfo.component.ts
@@ -7,23 +7,35 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class CheckoutContatinfoComponent {
   @Output() state = new EventEmitter<number>();
+  @Output() textChanged = new EventEmitter<{email:string,name:string,phonenumber:string}>();
 
-  email?: string;
-  name?: string;
-  phonenumber?: string;
+  email!: string;
+  name!: string;
+  phonenumber!: string;
 
   constructor() {
   }
 
   submit() {
-    console.log('Email:', this.email);
-    console.log('name:', this.name);
-    console.log('phone:', this.phonenumber);
+    // console.log('Email:', this.email);
+    // console.log('name:', this.name);
+    // console.log('phone:', this.phonenumber);
   }
   onClick() {
     // this.callbackFunction(1);
     if ((this.phonenumber && this.email && this.name)) {
-    this.state.emit(1);
+      this.state.emit(1);
+    }
   }
-}
+
+  sendTextToParent() {
+    this.textChanged!.emit({
+      email: this.email,
+      name: this.name,
+      phonenumber: this.phonenumber
+    });
+    console.log('Email:', this.email);
+    console.log('name:', this.name);
+    console.log('phone:', this.phonenumber);
+  }
 }
