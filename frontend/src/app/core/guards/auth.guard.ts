80260ace364714efb7ad72d0096a906a654339b5
Theerachot <b.theerachot891@hotmail.com>
2024-09-22 18:57:39 +0700

    Add sign-in and sign-out functionality with conditional rendering in the component

--- frontend/src/app/core/guards/auth.guard.ts
+++ frontend/src/app/core/guards/auth.guard.ts
@@ -5,7 +5,6 @@ import { UserService } from '@shared/services/user.service';
 export const authGuard: CanActivateFn = (route, state) => {
   let isauthenticated = inject(UserService).isAuthenticated();
   let router = inject(Router);
-  console.log(isauthenticated);
   if (isauthenticated) {
     return true;
   } else {
--- frontend/src/app/layout/nav/nav.component.ts
+++ frontend/src/app/layout/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from '@shared/services/user.service';
 
 interface CartItem {
   name: string;
@@ -16,21 +17,63 @@ interface CartItem {
 export class NavComponent {
   isModalOpen = false;
   cartItems: CartItem[] = [
-    { name: 'Snowfall', price: 70, img: 'images/Flowers/Fresh Flowers/Snowfall.webp' },
-    { name: 'Dawns Delight', price: 70, img: 'images/Flowers/Fresh Flowers/DawnsDelight.webp' },
-    { name: 'Pink Elegance', price: 70, img: 'images/Flowers/Fresh Flowers/PinkElegance.webp' },
-    { name: 'Rustic Charm', price: 70, img: 'images/Flowers/Fresh Flowers/RusticCharm.webp' },
-    { name: 'Autumn Symphony', price: 70, img: 'images/Flowers/Fresh Flowers/AutumnSymphony.webp' },
-    { name: 'Rosy Delight', price: 70, img: 'images/Flowers/Fresh Flowers/RosyDelight.webp' },
-    { name: 'Serenity', price: 89, img: 'images/Flowers/Fresh Flowers/Serenity.webp' },
-    { name: 'Blue Harmony', price: 55, img: 'images/Flowers/Fresh Flowers/BlueHarmony.webp' },
-    { name: 'Mystical Majesty', price: 80, img: 'images/Flowers/Fresh Flowers/MysticalMajesty.webp' },
-    { name: 'Blazing Blossoms', price: 75, img: 'images/Flowers/Fresh Flowers/BlazingBlossoms.webp' },
+    {
+      name: 'Snowfall',
+      price: 70,
+      img: 'images/Flowers/Fresh Flowers/Snowfall.webp',
+    },
+    {
+      name: 'Dawns Delight',
+      price: 70,
+      img: 'images/Flowers/Fresh Flowers/DawnsDelight.webp',
+    },
+    {
+      name: 'Pink Elegance',
+      price: 70,
+      img: 'images/Flowers/Fresh Flowers/PinkElegance.webp',
+    },
+    {
+      name: 'Rustic Charm',
+      price: 70,
+      img: 'images/Flowers/Fresh Flowers/RusticCharm.webp',
+    },
+    {
+      name: 'Autumn Symphony',
+      price: 70,
+      img: 'images/Flowers/Fresh Flowers/AutumnSymphony.webp',
+    },
+    {
+      name: 'Rosy Delight',
+      price: 70,
+      img: 'images/Flowers/Fresh Flowers/RosyDelight.webp',
+    },
+    {
+      name: 'Serenity',
+      price: 89,
+      img: 'images/Flowers/Fresh Flowers/Serenity.webp',
+    },
+    {
+      name: 'Blue Harmony',
+      price: 55,
+      img: 'images/Flowers/Fresh Flowers/BlueHarmony.webp',
+    },
+    {
+      name: 'Mystical Majesty',
+      price: 80,
+      img: 'images/Flowers/Fresh Flowers/MysticalMajesty.webp',
+    },
+    {
+      name: 'Blazing Blossoms',
+      price: 75,
+      img: 'images/Flowers/Fresh Flowers/BlazingBlossoms.webp',
+    },
   ];
   subtotal = 0;
   giftMessage = '';
-
-  constructor(private router: Router) {}
+  isauthenticated: boolean;
+  constructor(private router: Router, private user: UserService) {
+    this.isauthenticated = user.isAuthenticated();
+  }
 
   openModal() {
     this.calculateSubtotal(); // Calculate subtotal when modal opens
@@ -47,7 +90,10 @@ export class NavComponent {
   }
 
   calculateSubtotal() {
-    this.subtotal = this.cartItems.reduce((total, item) => total + item.price, 0);
+    this.subtotal = this.cartItems.reduce(
+      (total, item) => total + item.price,
+      0
+    );
   }
 
   scrollToContact() {
@@ -57,5 +103,8 @@ export class NavComponent {
         element.scrollIntoView({ behavior: 'smooth' });
       }
     });
+  }
+  onSignOut() {
+    this.user.signout().subscribe();
   }
 }
