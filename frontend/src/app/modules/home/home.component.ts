98fad45444b112209faf477ae21e07399d2dd29c
Theerachot <b.theerachot891@hotmail.com>
2024-09-23 03:21:01 +0700

    Additional Animate On Scroll at home page

--- frontend/src/app/modules/home/home.component.ts
+++ frontend/src/app/modules/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ProductCategoryService } from '@shared/services/productCategory/product-category.service';
 import { IProductCategory } from '../../../types/productCategory';
 import { ArrayBufferToBase64 } from '../../../lib/index';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -13,6 +15,15 @@ export class HomeComponent implements OnInit {
   constructor(private httpService: ProductCategoryService) {}
 
   ngOnInit() {
+    AOS.init({
+      mirror: true,
+      once: false,
+      easing: 'ease',
+      duration: 750,
+    });
+    setTimeout(() => {
+      AOS.refreshHard();
+    }, 250);
     this.httpService.getProductCategory().subscribe(
       (response) => {
         this.items = response.data;
