ed72ffe91690edaf8b6b8bbb751f33a2526a68e1
Theerachot <b.theerachot891@hotmail.com>
2024-09-23 20:05:14 +0700

    add page loading

---
+++ frontend/src/app/modules/page-loading/page/page-loading.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-loading',
+  templateUrl: './page-loading.component.html',
+  styleUrl: './page-loading.component.css',
+})
+export class PageLoadingComponent {}
